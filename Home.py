d32a602fc21546ba9934e443ac08979d4ff6b727
Luís Luz <lcnevesluz@gmail.com>
2025-06-24 09:14:21 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+
+st.sidebar.image("images/logo.png", use_column_width=True)
+
 st.set_page_config(page_title="App de Geotecnia", layout="centered")
 
 st.title("Cálculos genericos de Geotecnia")
