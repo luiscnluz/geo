ea3e7097b243d9606e79c76dd21967f7b3d6bb3e
Luís Luz <lcnevesluz@gmail.com>
2025-06-25 15:59:57 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-st.sidebar.image("images/logo.png", use_container_width=True)
+#st.sidebar.image("images/logo.png", use_container_width=True)
 
 
 st.set_page_config(page_title="App de Geotecnia", layout="centered")
